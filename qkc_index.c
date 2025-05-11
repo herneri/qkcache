53d040718ead993f17298d1941a0767382d64b43
Eric Hernandez <herneri@protonmail.com>
2025-05-11 11:34:37 -0500

    style: Improve function names

--- qkc_index.c
+++ qkc_index.c
@@ -5,7 +5,7 @@
 #include "qkc_db.h"
 #include "qkc_index.h"
 
-struct index_entry *qkc_load_index_entry(struct qkc_database *database) {
+struct index_entry *qkc_recent_index_entry(struct qkc_database *database) {
 	struct index_entry *loaded_entry = malloc(sizeof(struct index_entry));
 	if (loaded_entry == NULL) {
 		fprintf(stderr, "qkcache: Not enough memory to load recent index entry\n");
@@ -25,15 +25,13 @@ struct index_entry *qkc_load_index_entry(struct qkc_database *database) {
 	return loaded_entry;
 }
 
-bool qkc_write_index_entry(struct qkc_database *database, const float data_size) {
-	struct index_entry *recent_entry = qkc_load_index_entry(database);
+bool qkc_append_index_entry(struct qkc_database *database, const float data_size) {
+	struct index_entry *recent_entry = qkc_recent_index_entry(database);
 	struct index_entry new_entry;
 
 	new_entry.entry_number = database->entry_count;
 	new_entry.start_bytes = (recent_entry != NULL) ? recent_entry->end_bytes + 1 : sizeof(int);
 	new_entry.end_bytes = new_entry.start_bytes + data_size;
-
-	printf("%f end: %f \n", new_entry.start_bytes, new_entry.end_bytes);
 
 	fseek(database->index_file, sizeof(int), 1);
 	fseek(database->index_file, sizeof(struct index_entry), database->entry_count);
