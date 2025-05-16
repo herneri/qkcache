6b08a54e4c01b52a7ed48011d4b011acf5dfe858
Eric Hernandez <herneri@protonmail.com>
2025-05-15 21:15:12 -0500

    docs: Prepend license notice

--- qkc_index.h
+++ qkc_index.h
@@ -1,4 +1,23 @@
 /*
+	qkc_index.h: Index entry management, meant strictly for use with qkcache engine.
+
+	Copyright (C) 2025  Eric Hernandez
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+ 	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
         NOTICE: This header file is intended to only be used by the qkcache engine
         and is not meant for use by the client program. Using these functions will
         certainly cause severe, irreversible errors if used incorrectly.
